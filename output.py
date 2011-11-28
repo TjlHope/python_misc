08f7291883b0761194c75e3c8823997dda71ec45
Tom JL Hope <tjlhope@gmail.com>
2011-11-28 19:52:12 +0000

    Add latex representation of table in table (numpy).

--- output.py
+++ output.py
@@ -2,12 +2,88 @@
 """
 
 import sys
-import os
+#import os
 import logging
 #import csv
 
-from wrappers import retry_open, ls
+from wrappers import s, header_join, ls, retry_open
 import unicode_csv as csv
+
+
+def repr2Dof2D(table, row_headers=None, column_headers=None, sep=None,
+               minor_sep=None, header_sep=None):
+    """Gives a string representation of a 2D array of 2D arrays."""
+    # Some pretty defaults
+    if header_sep is None:
+        header_sep = ('\t|| ', '\n========\n') if sep is None else sep
+    if minor_sep is None:
+        minor_sep = (', ', '\n') if sep is None else sep
+    if sep is None:
+        sep = ('\t| ', '\n--------\n')
+    # Main body.
+    if column_headers:          # Add the top row
+        if row_headers and len(column_headers) == len(table[0]):
+            # Add the blank square in the top left
+            rows = [header_join([''] + column_headers, header_sep[0], sep[0])]
+        else:
+            rows = [header_join(column_headers, header_sep[0], sep[0])]
+    else:
+        rows = []
+    for i, row in enumerate(table):
+        cell_rows = []  # Initiate list with rows of each cell concat'ed.
+        for cell in row:
+            for ii, cell_row in enumerate(cell):
+                if len(cell_rows) <= ii:        # if first column, initiate row
+                    if row_headers:
+                        # Add the header or a blank filler.
+                        cell_rows.append([row_headers[i]] if ii == 0 else [''])
+                    else:
+                        cell_rows.append([])
+                cell_rows[ii].append(minor_sep[0].join(s(cell_row)))
+        rows.append(minor_sep[1].join([header_join(r, header_sep[0], sep[0])
+                                        for r in cell_rows]))
+    return header_join(rows, header_sep[1], sep[1])
+
+
+def latex2Dof2D(table, row_headers=None, column_headers=None, sep=None,
+                minor_sep=None, header_sep=None):
+    """Wrapper around repr2Dof2D() for LaTeX tabular environments."""
+    # Some pretty defaults (requires booktabs package.
+    if header_sep is None:
+        if sep is None:
+            if column_headers:      # Need a pretty header split.
+                # Add the blank square in the top left
+                cols = table.shape[1]
+                inner_cols = table[0, 0].shape[1]
+                inc = (2 if row_headers and len(column_headers) == cols else 1)
+                mid = ' '.join(['\\cmidrule(lr){%s-%s}' %
+                                    (i, i + inner_cols - 1)
+                                    for i in range(inc, cols * inner_cols + 1,
+                                                   inner_cols)])
+            else:
+                mid = '\\midrule'
+            header_sep = ('\t& ', ''.join(('\t\\\\ ', mid, '\n')))
+        else:
+            header_sep = sep
+    if minor_sep is None:
+        minor_sep = ('\t& ', '\t\\\\\n') if sep is None else sep
+    if sep is None:
+        sep = ('\t& ', '\t\\\\ \\addlinespace\n')
+    # Make LaTeX tabular header.
+    header = '\\begin{tabular}{'
+    if row_headers:
+        header += 'l '
+    header += ' '.join([' '.join(['r'] * table[0, 0].shape[1])] *
+                        table.shape[1])
+    header += '}\n\\toprule'
+    # Make headers span correct number of cells.
+    r_h = map(lambda r: '\\multirow{%s}{*}{%s}' % (table[0, 0].shape[0], r),
+              row_headers)
+    c_h = map(lambda c: '\\multicolumn{%s}{c}{%s}' % (table[0, 0].shape[1], c),
+              column_headers)
+    str_repr = repr2Dof2D(table, r_h, c_h, sep, minor_sep, header_sep)
+    footer = '\t\\\\ \\bottomrule\n\\end{tabular}'
+    return '\n'.join((header, str_repr, footer))
 
 
 class OutputBase(object):
@@ -24,7 +100,7 @@ class OutputBase(object):
         self.file_name = fl
         self.log.debug("Opened {0} for writing.".format(fl))
         return self.file
-    
+
     def write(self, message):
         print(message)
 
@@ -38,6 +114,7 @@ class OutputBase(object):
         #if not self.file.closed:
             #self.close()
 
+
 class OutputCsv(OutputBase):
     def __init__(self, *args, **kwargs):
         super(self.__class__, self).__init__(*args, **kwargs)
@@ -47,27 +124,31 @@ class OutputCsv(OutputBase):
         else:
             self.file_name = '.'.join((self.name, 'csv'))
         self.open(mode='wb')
-    
+
     def open(self, *args, **kwargs):
         super(self.__class__, self).open(*args, **kwargs)
         self.csv = csv.writer(self.file, lineterminator='\n')
         return self.file
-    
+
     def writerow(self, row):
         if self.file.tell() == 0:
             if hasattr(row, '_fields'):
                 self.csv.writerow(row._fields)
         self.csv.writerow(row)
 
+
 try:
     import xlwt
+
     class OutputXl(OutputBase):
         pass
 except ImportError:
     pass
 
+
 try:
     from htmlgen import HTMLgen
+
     class OutputHtml(OutputBase):
         def __init__(self, *args, **kwargs):
             super(self.__class__, self).__init__(*args, **kwargs)
@@ -77,7 +158,7 @@ try:
             else:
                 self.file_name = '.'.join((self.name, 'html'))
             self.open(mode='wb')
-            
+
         def open(self, title=None, *args, **kwargs):
             super(self.__class__, self).open(*args, **kwargs)
             if title == None:
@@ -87,20 +168,21 @@ try:
             self.table.body = []
             self.html.append(self.table)
             return self.file
-            
+
         def writerow(self, row):
             if not self.table.heading:
                 if hasattr(row, '_fields'):
                     self.table.heading = row._fields
             self.table.body.append(list(row))
-            
-        def close(self, fl= None, *args, **kwargs):
+
+        def close(self, fl=None, *args, **kwargs):
             if fl == None:
                 fl = self.file
             fl.write(str(self.html))
             super(self.__class__, self).close(fl, *args, **kwargs)
 except ImportError:
     pass
+
 
 def table(rows, name='output', types=('csv',)):
     log = logging.getLogger(__name__.title())
@@ -113,3 +195,7 @@ def table(rows, name='output', types=('csv',)):
         [output.writerow(row) for output in outputs]
     [output.close() for output in outputs]
 
+
+if __name__ == '__main__':
+    sys.stderr.write('\n'.join((__doc__, 'import only.', '')))
+    sys.exit(1)
--- wrappers.py
+++ wrappers.py
@@ -1,4 +1,4 @@
-""" wrappers/__init__.py
+""" wrappers.py
     Useful Wrapper variables, functions, or classes.
 
     @author: TjlHope
@@ -8,34 +8,57 @@ import sys
 import time
 import codecs
 import re
+from collections import Iterable
 
 import urllib2
 import logging
 
+
 log = logging.getLogger(__name__)
+
 
 def timestamp():
     """ Nice timestamp format.
     """
     return time.strftime("%Y-%m-%d %H:%M:%S")
 
+
+def s(i):
+    """Function for intelligent string conversion within iterables."""
+    if isinstance(i, basestring):
+        return i
+    elif isinstance(i, Iterable):
+        return map(str, i)
+    else:
+        return str(i)
+
+
+def header_join(iterable, header_sep, sep=None):
+    """Joins an iterable with a different string between the first elements."""
+    if sep is None:
+        sep = header_sep
+    return header_sep.join((s(iterable[0]), sep.join(s(iterable[1:]))))
+
+
+def ls(iterable):
+    """ Nicely display an iterable as a string, in brackets only if there are
+        multiple values.
+    """
+    iter_strs = [str(i) for i in iterable]
+    string = iter_strs[0] if len(iter_strs) is 1 else "', '".join(iter_strs)
+    return string.join(("'", "'"))
+
+
 delchars = ''.join(c for c in map(chr, xrange(256)) if not c.isalnum())
 def key(k, *args):
-    """ Strips spacing chars to give a nice dict key str. Any extra arguments 
-        given are also removed from the string
+    """ Strips spacing chars to give a nice dict key str. Any extra arguments
+        given are also removed from the string.
     """
     k = str(k).lower()
     for arg in args:
         k = k.replace(arg, '')
     return k.translate(None, delchars)
 
-def ls(iterable):
-    """ Nicely display an iterable as a string, in brackets only if there are 
-        multiple values.
-    """
-    iter_strs = [str(i) for i in iterable]
-    string = iter_strs[0] if len(iter_strs) is 1 else "', '".join(iter_strs)
-    return string.join(("'", "'"))
 
 def unkey(k, key_type=None, sep=' ', case='lower', keywords=[]):
     """ Attempts to reconstruct a str that has been 'key()'ed.
@@ -56,6 +79,7 @@ def unkey(k, key_type=None, sep=' ', case='lower', keywords=[]):
     # return it with 'case' case
     return getattr(k, case)()
 
+
 def display_TraceBack(logger=logging.getLogger('TB'), level='error'):
     """ Allow the logging of a Traceback to a file.
     """
@@ -71,11 +95,12 @@ def display_TraceBack(logger=logging.getLogger('TB'), level='error'):
         function_name = exc_tb.tb_frame.f_code.co_name
         file_name = exc_tb.tb_frame.f_code.co_filename
         line_no = exc_tb.tb_lineno
-        log(filename+" - \'"+function_name+"\' - "+str(line_no))
+        log(''.join((file_name, " - '", function_name, "' - ", str(line_no))))
         exc_tb = exc_tb.tb_next
 
+
 def retry_open(fl, *args, **kwargs):
-    """ Allows you to retry opening the file if theres an IOError so that 2 
+    """ Allows you to retry opening the file if theres an IOError so that 2
         hour processing run isn't wasted.
     """
     while True:
@@ -87,6 +112,7 @@ def retry_open(fl, *args, **kwargs):
             if 's' not in sys.stdin.readline().lower().strip():
                 continue
 
+
 def retry_urlopen(url, attempts=50, *args, **kwargs):
     """ Keep retrying the connection in-case the network's down.
     """
@@ -94,19 +120,21 @@ def retry_urlopen(url, attempts=50, *args, **kwargs):
         try:
             return urllib2.urlopen(url, *args, **kwargs)
         except urllib2.URLError as ex:
-            log.error("Caught {0} in GetPage, retry number {1}.".format(ex, attempt))
+            log.error("Caught {0} in GetPage, retry number {1}."
+                        .format(ex, attempt))
             if "590" in str(ex):
                 log.error("Getting new cookie")
                 GetCookie()
             continue
-            
+
     log.error("Retried {0} times with no valid response.".format(attempts))
     raise urllib2.URLError("Open retried {0} times!".format(attempts))
+
 
 html_tag_re = re.compile(r'<.*?>')
 def strip_html(data):
     return html_tag_re.sub('', data)
 
 if __name__ == '__main__':
-    sys.stderr.write("This module is to be imported, do not use directly.")
+    sys.stderr.write('\n'.join((__doc__, 'import only.', '')))
     sys.exit(1)
